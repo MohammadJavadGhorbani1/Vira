cb74b863700f1962416073f3de8583cf5f880ecd
MohammadJavad <ghorbanimohammadjavad969@gmail.com>
2024-07-19 19:20:27 +0430

    sitesettings created

--- home_module/views.py
+++ home_module/views.py
@@ -2,26 +2,32 @@ from django.shortcuts import render
 from django.views import View
 from django.views.generic import TemplateView
 from product_module.models import *
+from sitesettings_module.models import *
 
 # Create your views here.
 
-# class HomePage(View):
-#     def get(self , request):
-#         return render(request , 'home.html' , {
-#
-#         })
+class HomePage(View):
+    def get(self , request):
+        sliders = SliderSettingsModel.objects.filter(is_active=True)
+        ads = AdsSettingsModel.objects.filter(is_active=True).first()
+        return render(request , 'home.html' , {
+            'sliders':sliders,
+            'ads':ads,
+        })
 
-class HomePage(TemplateView):
-    template_name = 'home.html'
-    def get_context_data(self, **kwargs):
-        context = super(HomePage , self).get_context_data(**kwargs)
-        return context
+# class HomePage(TemplateView):
+#     template_name = 'home.html'
+#     def get_context_data(self, **kwargs):
+#         context = super(HomePage , self).get_context_data(**kwargs)
+#         return context
 
 def header_render_partial(request):
+    settings = SiteSettingsModel.objects.filter(is_active=True).first()
     categories = ProductCategory.objects.filter(parent_category=None)
     # child_categories = ProductCategory.objects.filter(parent_category=not None)
     return render(request , 'header.html' , {
         'categories':categories,
+        'settings':settings,
         # 'child_categories':child_categories,
     })
 
@@ -33,6 +39,7 @@ def header_render_partial(request):
 #         })
 
 def footer_render_partial(request):
+    settings = SiteSettingsModel.objects.filter(is_active=True).first()
     return render(request , 'footer.html' , {
-
+        'settings':settings,
     })
--- sitesettings_module/models.py
+++ sitesettings_module/models.py
@@ -2,13 +2,36 @@ from django.db import models
 
 # Create your models here.
 
-# class SiteSettingsModel(models.Model):
-#     logo = models.ImageField(upload_to='logo' , verbose_name='لوگوی سایت')
-#     phone = models.CharField(max_length=20 , verbose_name='شماره تماس با ما')
-#     email = models.EmailField(verbose_name='ایمیل تماس با ما')
-#     about_us = models.TextField(verbose_name='متن درباره ما')
-#     def __str__(self):
-#         return self.phone
-#     class Meta:
-#         verbose_name = 'تنظیم'
-#         verbose_name_plural = 'تنظیمات'
+class SiteSettingsModel(models.Model):
+    logo = models.ImageField(upload_to='logo' , verbose_name='لوگوی سایت')
+    phone = models.CharField(max_length=20 , verbose_name='شماره تماس با ما')
+    email = models.EmailField(verbose_name='ایمیل تماس با ما')
+    location = models.CharField(max_length=200 , verbose_name='مکان')
+    about_us = models.TextField(verbose_name='متن درباره ما')
+    is_active = models.BooleanField(verbose_name='فعال باشد/نباشد')
+    def __str__(self):
+        return self.phone
+    class Meta:
+        verbose_name = 'تنظیم'
+        verbose_name_plural = 'تنظیمات'
+
+class SliderSettingsModel(models.Model):
+    image = models.ImageField(upload_to='sliders' , verbose_name='تصویر اسلایدر')
+    link = models.URLField(max_length=200 , verbose_name='لینک')
+    is_active = models.BooleanField(verbose_name='فعال باشد/نباشد')
+    def __str__(self):
+        return self.link
+    class Meta:
+        verbose_name = 'اسلایدر'
+        verbose_name_plural = 'اسلایدرها'
+
+class AdsSettingsModel(models.Model):
+    image = models.ImageField(upload_to='ads' , verbose_name='تصویر')
+    link = models.URLField(max_length=200 , verbose_name='لینک')
+    is_active = models.BooleanField(verbose_name='فعال باشد/نباشد')
+    def __str__(self):
+        return self.link
+    class Meta:
+        verbose_name = 'تبلیغ'
+        verbose_name_plural = 'تبلیغات'
+
